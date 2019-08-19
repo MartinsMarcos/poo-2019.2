28a2a6db96134a76f75d0bb517bd72fa42b7d2c3
Marcos Vitor <42944191+MartinsMarcos@users.noreply.github.com>
2019-08-19 19:44:11 -0300

    meu primeiro programa

---
+++ S01ep01/src/Inhainmundo.java
@@ -0,0 +1,9 @@
+
+public class Inhainmundo {
+
+	public static void main(String[] args) {
+		System.out.println("Relou mundo da programacao, um code real é vc escrever compilar salvar e da erro");
+
+	}
+
+}
