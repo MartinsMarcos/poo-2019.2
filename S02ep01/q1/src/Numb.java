d06f3a9ebefb220e1b978ed35214fb552cb01b6d
Marcos Vitor <42944191+MartinsMarcos@users.noreply.github.com>
2019-08-19 22:10:07 -0300

    exercicios apostila

---
+++ S02ep01/q1/src/Numb.java
@@ -0,0 +1,9 @@
+public class Numb {
+	public static void main(String[] args) {
+		for (int i = 150; i <= 300; i++) {
+			System.out.println(i);		
+		}
+
+	}
+}
+
---
+++ S02ep01/q1/src/num.java
@@ -0,0 +1,11 @@
+public class Num {
+	public static void main(String[] args) {
+		for (int i = 150; i <= 300; i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+}
+
+
