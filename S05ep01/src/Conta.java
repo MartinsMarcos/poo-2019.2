4a5de3fc67555bef734d1ade474e0061afb3d594
Marcos Vitor <42944191+MartinsMarcos@users.noreply.github.com>
2019-09-09 00:14:32 -0300

    conta e projeto

--- S05ep01/src/Conta.java
+++ S05ep01/src/Conta.java
@@ -1,45 +1,82 @@
 
 public class Conta {
-	private String titular;
-	private String numero;
-	private double saldo; 
-	int id;	
-	static int identificador = ();
-				
-		}
-		public Conta (string titular, int numero, double saldo ) {
-			this.titular = titular;
-			this.numero = numero;
-			this.saldo = saldo;
-			
-		public Conta () {
-			this.id = identificador;
-			identificador +=1;
-				
-		}
-			
-	
-	public double getSaldo() {
-		return saldo;
-	}
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	public String getTitular() {
-		return titular;
-	}
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
-	
-	public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	
-	
-	
+    private String titular;
+    static int nextId = 1;
+    private int id;
+    private double saldo;
 
+
+
+    public Conta(String titular, double saldo){//int totalDeContas){
+        this.titular = titular;
+        this.saldo = saldo;
+        this.id= nextId;
+        nextId += 1;
+        this.totalDeContas = totalDeContas;
+    }
+    public Conta(){
+
+    }
+    public String toString(){
+        return "[" + this.titular + "]" +
+               "Numero da conta: " + this.id + "Saldo:" + this.saldo;
+    }
+
+    //Questão 2:
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getIdentificador() {
+        return id;
+    }
+
+    void getRedimento(){
+
+    }
+    void sacar(double valor){
+        if (titular != null){
+            if(valor <= this.saldo){
+                System.out.println("Sacando dinheiro" + valor + "...");
+                this.saldo = this.saldo - valor;
+            } else{
+                System.out.println("Você não tem saldo suficiente para realizar saque ");
+            }
+        } else {
+            System.out.println("So é possivel realizar saque por usuarios com contas ativas");
+        }
+    }
+
+    void depositar(double valor){
+        if (titular != null){
+            System.out.println("Realizando deposito" + valor + "...");
+            this.saldo = this.saldo + valor;
+        } else {
+            System.out.println("So é possivel realizar saque por usuarios com contas ativas");
+        }
+    }
+
+    void tranferir(int id, double valor){
+        if (titular != null){
+            if(valor <= this.saldo){
+                System.out.println("Realizando transferencia" + valor + "...");
+                this.saldo -= valor;
+            } else{
+                System.out.println("Você não tem saldo suficiente para realizar tranferencia desse valor");
+            }
+        } else {
+            System.out.println("So é possivel realizar transferencia por usuarios com contas ativas");
+        }
+    }
+    void receber(double valor){
+        this.saldo = valor;
+    }
 }
--- S05ep01/src/Controler.java
+++ S05ep01/src/Controler.java
@@ -1,21 +1,30 @@
 
-public class Controler {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		
-		Conta conta1 = new Conta ();
-		System.out.println(conta2.identificador);
-		
-		Conta conta3 = new Conta (String titular);
-		
-		Conta conta2 = new Conta ();
-		conta2.setTitular("Marcos");
-		System.out.println(conta2.getTitular());
-		conta2.setSaldo(100);
-		System.out.println(conta2.getSaldo());
-		conta2.setNumero("1425135");
-		System.out.println(conta2.getNumero());
-		
-	}
-
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
+        System.out.println("Opções: iniciar [nome, saldo], sacar [valor], depositar [valor], transferir [id e valor], status, fim");
+        while (true){
+            String line = scanner.nextLine();
+            String[] ui = line.split(" ");
+            if (ui[0].equals("fim")){
+                System.out.println("Encerrando ...");
+                break;
+            } else if (ui[0].equals("iniciar")){
+                conta = new Conta(ui[1], Integer.parseInt(ui[2]));
+                System.out.println("Conta iniciada com sucesso!\n" + conta);
+            } else if (ui[0].equals("status")) {
+                System.out.println(conta);
+            } else if (ui[0].equals("sacar")){
+                conta.sacar(Integer.parseInt(ui[1]));
+            } else if (ui[0].equals("depositar")) {
+                conta.depositar(Integer.parseInt(ui[1]));
+            } else if (ui[0].equals("tranferir")) {
+                conta.tranferir(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]));
+                conta.receber(Integer.parseInt(ui[1]));
+            }
+        }
+    }
 }
